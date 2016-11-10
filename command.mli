8bf9fa298bdfadce5f238eae469c20c7a553c2d6
M <mcg79@cornell.edu>
2016-11-09 22:17:48 -0500

    adding MLI files

---
+++ command.mli
@@ -0,0 +1,46 @@
+(* Command Module --- types and functions for cml commands *)
+
+(* A variant type for the commands supported by cml control. *)
+type command =
+| Add | Branch | Checkout | Commit | Diff | Help | Init | Merge | Rm
+| Stash | Status
+
+(* a record type for the input a user gives to the terminal.
+ * command is the desired command, arg is what the user wants to run the command
+ * on, and flags are any flags to modify the command.
+ *)
+type input = {
+  command: command;
+  arg: string;
+  flags: string list
+}
+
+(* parses a string and returns an input type *)
+val parse_input : string -> input
+(* executes a command *)
+val execute     : input -> unit
+
+(* add file contents to the index. *)
+val add      : input -> unit
+(* creates a new branch *)
+val branch   : string -> string list -> unit
+(* checkout Switch branches or restore working tree files. *)
+val checkout : string -> string list -> unit
+(* commit Record changes to the repository.
+ * Stores the current contents of the index in a new commit
+ * along with a log message from the user describing the changes. *)
+val commit   : string -> string list -> unit
+(* diff Show changes between commits, commit and working tree, etc *)
+val diff     : string -> string list -> unit
+(* Display help information about CmlControl. *)
+val help     : string -> string list -> unit
+(* init Create an empty CmlControl repository. *)
+val init     : string -> string list -> unit
+(* merge Join two or more development histories together. *)
+val merge    : string -> string list -> unit
+(* Remove files from working tree and from the index. *)
+val rm       : string -> string list -> unit
+(* stash Stash the changes in a dirty working directory away. *)
+val stash    : string -> string list -> unit
+(* displays any changes to the working directory *)
+val status   : string -> string list -> unit
---
+++ utils.mli
@@ -0,0 +1,62 @@
+(* Utils Module --- helper functions for directory data structure *)
+
+
+(* a record representing all the information about a commit type.
+ * note: pointers in this case are string file names.
+ *)
+type commit = {
+  author: string;
+  message: string;
+  date: string;
+  tree_ptr: string;
+  prev_commit_ptr: string;
+}
+
+(* type blob represents a compressed file object *)
+type blob = string
+(* type tree represents a directory with pointers to blobs/other trees
+ * note: pointers in this case are string file names
+ *)
+type tree = string list
+
+(* type mapping is a mapping between a filename and its hash *)
+type mapping = string * string
+(* type index is a list of mappings, referred to as the index in git *)
+type index = mapping list
+
+(* a variant for an cml object, which can be a Blob, Tree, or Commit  *)
+type object =
+| Blob of blob
+| Tree of tree
+| Commit of commit
+
+
+
+(* hash returns a SHA-1 hash of a given input *)
+val hash : string -> string
+(* compress compresses a file/directory
+ * takes initial path and final path as arguments.
+ *)
+val compress: string -> string -> unit
+(* decompress decompresses a file/directory
+ * takes initial and final path as arguments.
+ *)
+val decompress: string -> string -> unit
+(* creates and object in the object directory and returns its name (hashed) *)
+val create_object: object -> string
+(* takes hash and returns an object type *)
+val parse_object: string -> object
+(* returns the current HEAD of the cml repository *)
+val get_head: unit -> string
+(* sets the HEAD of the cml repository *)
+val set_head: string -> unit
+(* returns the HASH of a head of the given branch *)
+val get_branch_ptr: string -> string
+(* initializes a given commit to a given branch name *)
+val set_branch_ptr: string -> string -> unit
+(* updates the index by adding a mapping type *)
+val update_index: index -> mapping -> index
+(* returns teh index *)
+val get_index: unit -> index
+(* initializes an index in the cml directory *)
+val set_index: index -> unit
