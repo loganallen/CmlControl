66effe1921884bb0c5e71ff6c80210fa079025da
M <mcg79@cornell.edu>
2016-11-09 22:17:59 -0500

    and add....

---
+++ print.mli
@@ -0,0 +1,13 @@
+(* Print Module --- prints to the terminal *)
+
+
+(* prints a string in the color specified *)
+val print_color: string -> string -> unit
+(* prints an error message in error format *)
+val print_error: string -> string -> unit
+(* prints a specified amount of indentations *)
+val print_indent: string -> int -> unit
+(* prints a newline *)
+val print_newline: unit -> unit
+(* prints the camel emoji *)
+val print_camel: unit -> unit
