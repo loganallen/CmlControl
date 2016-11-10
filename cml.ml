eac06749e8a017e68806831b0ae4d851cb3f687e
Andrew Grossfeld <amg445@cornell.edu>
2016-11-10 18:36:07 -0500

    repo clean up, updated cml and cml.ml to pass and print args to main

--- cml.ml
+++ cml.ml
@@ -1,2 +1,5 @@
 let () =
-  print_endline "cml started - lets gooooo"
+  print_endline "cml started - below are the passed in args";
+  for i = 1 to Array.length Sys.argv - 1 do
+    print_endline Sys.argv.(i)
+  done;;
