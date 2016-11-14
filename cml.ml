2891099188f0745c5bb5fd9b63027b66792530b6
Andrew Grossfeld <amg445@cornell.edu>
2016-11-14 12:41:18 -0500

    added color - connect in main

--- cml.ml
+++ cml.ml
@@ -1,7 +1,8 @@
 open Command
+open Print
 
 let () =
   print_endline "cml started - below are the passed in args";
   for i = 1 to Array.length Sys.argv - 1 do
-    print_endline Sys.argv.(i)
+    print_color Sys.argv.(i) "yellow"
   done;;
--- print.ml
+++ print.ml
@@ -1,15 +1,16 @@
 (* prints a string in the color specified *)
 let print_color (msg : string) (color : string) : unit =
-  let m = ("\n" ^ msg ^ "\n") in
+  let m = (msg ^ "\n") in
   match color with
     | "red" -> ANSITerminal.(print_string [red] m)
     | "green" -> ANSITerminal.(print_string [green] m)
     | "blue" -> ANSITerminal.(print_string [blue] m)
+    | "yellow" -> ANSITerminal.(print_string [yellow] m)
     | _ -> ANSITerminal.(print_string [red] m)
 
 (* prints an error message in error format *)
 let print_error (msg : string) (options : string) : unit =
-  ANSITerminal.(print_string [red] ("\n" ^ msg ^ "\n"))
+  ANSITerminal.(print_string [red] (msg ^ "\n"))
 
 (* prints a specified amount of indentations *)
 let rec print_indent (msg : string) (num_indents : int) : unit =
