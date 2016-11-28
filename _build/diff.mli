f301fc6af6ea748daf848cb82b118ea516f20109
ncskier <bcw58@cornell.edu>
2016-11-28 13:31:54 -0500

    make clean

--- _build/diff.mli
+++
@@ -1,5 +0,0 @@
-(* prints a diff between two files to console *)
-val print_diff_files : string -> string -> unit
-
-(* prints diffs all pairs of files in a list *)
-val print_diff_files_mult : (string * string) list -> unit
